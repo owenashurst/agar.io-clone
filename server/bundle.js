22fbd022bab41c2f543e6d6b0b727d7e5f16a0d5
christianalfoni <christianalfoni@gmail.com>
2015-04-28 10:59:10 +0200

    Added timing to bundling

--- server/bundle.js
+++ server/bundle.js
@@ -7,9 +7,14 @@ var mainPath = path.resolve(__dirname, '..', 'app', 'main.js');
 
 module.exports = function () {
 
+  var bundleStart = null;
   var compiler = webpack(webpackConfig);
-  compiler.plugin("done", function() {
-    console.log('Project is ready!');
+  compiler.plugin('compile', function() {
+    console.log('Bundling...');
+    bundleStart = Date.now();
+  });
+  compiler.plugin('done', function() {
+    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
   });
 
   var bundler = new WebpackDevServer(compiler, {
