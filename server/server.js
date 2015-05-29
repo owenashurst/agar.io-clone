8c54ee6bd6cccd9787530aed4922290c8ee133fb
Christian Schwarz <christian.schwarz@refactory.at>
2015-05-29 11:13:14 +0200

    Fixed jshint errors

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ function randomColor() {
     return {
         fill: color,
         border: '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-    }
+    };
 }
 
 function findPlayer(id) {
@@ -101,14 +101,14 @@ function hitTest(start, end, min) {
 
 // From giongto35/agar.io-clone
 function movePlayer(player, target) {
-    var dist = Math.sqrt((target.y - player.screenHeight / 2) * (target.y - player.screenHeight / 2) + (target.x - player.screenWidth / 2) * (target.x - player.screenWidth / 2))
+    var dist = Math.sqrt((target.y - player.screenHeight / 2) * (target.y - player.screenHeight / 2) + (target.x - player.screenWidth / 2) * (target.x - player.screenWidth / 2)),
         deg = Math.atan2(target.y - player.screenHeight / 2, target.x - player.screenWidth / 2),
         deltaY = player.speed * Math.min(1, dist / (defaultPlayerSize + player.mass)) * Math.sin(deg),
         deltaX = player.speed * Math.min(1, dist / (defaultPlayerSize + player.mass)) * Math.cos(deg);
     // This code is for moving in a screen
     // deltaY = deltaY > 0 ? deltaY = Math.min(deltaY, target.y - player.y) : deltaY = Math.max(deltaY, target.y - player.y)
     // deltaX = deltaX > 0 ? deltaX = Math.min(deltaX, target.x - player.x) : deltaX = Math.max(deltaX, target.x - player.x)
-    
+
     player.y += ((player.y > 0 || player.y < 0 && deltaY > 0) && (player.y < player.gameHeight || player.y > player.gameHeight && deltaY < 0)) ? deltaY : 0;
     player.x += ((player.x > 0 || player.x < 0 && deltaX > 0) && (player.x < player.gameWidth || player.x > player.gameWidth && deltaX < 0)) ? deltaX : 0;
 }
@@ -126,7 +126,7 @@ io.on('connection', function (socket) {
         player.id = userID;
         sockets[player.id] = socket;
 
-        if (findPlayer(player.id) == null) {
+        if (findPlayer(player.id) === null) {
             console.log('Player ' + player.id + ' connected!');
             users.push(player);
             currentPlayer = player;
@@ -163,7 +163,7 @@ io.on('connection', function (socket) {
     socket.on('playerSendTarget', function (target) {
      // if you want to use uncomment the line below
     //    console.log(currentPlayer.x + " " + currentPlayer.y);
-        if (target.x != currentPlayer.x || target.y != currentPlayer.y) {
+        if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
             movePlayer(currentPlayer, target);
 
             for (var f = 0; f < foods.length; f++) {
@@ -199,7 +199,7 @@ io.on('connection', function (socket) {
                         {x: currentPlayer.x, y: currentPlayer.y},
                         currentPlayer.mass + defaultPlayerSize
                     )) {
-                    if (users[e].mass != 0 && users[e].mass < currentPlayer.mass - eatableMassDistance) {
+                    if (users[e].mass !== 0 && users[e].mass < currentPlayer.mass - eatableMassDistance) {
                         if (currentPlayer.mass < maxSizeMass) {
                             currentPlayer.mass += users[e].mass;
                         }
@@ -228,7 +228,7 @@ io.on('connection', function (socket) {
 // Don't touch on ip
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
 var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
-if (process.env.OPENSHIFT_NODEJS_IP != undefined) {
+if (process.env.OPENSHIFT_NODEJS_IP !== undefined) {
     http.listen( serverport, ipaddress, function() {
         console.log('listening on *:' + serverport);
     });
