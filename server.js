a8bf5a327c0987e2bfe8c44962d044d6ac82b60d
Christian Alfoni <christianalfoni@gmail.com>
2015-05-30 09:16:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var proxy = httpProxy.createProxyServer({
 }); 
 var app = express();
 var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? 8080 : 3000;
+var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
