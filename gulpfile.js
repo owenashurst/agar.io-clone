170294e0f4c054545647f607cf4a88c1208b242c
Usuń To <RemoveIt@users.noreply.github.com>
2015-05-31 21:02:04 +0200

    Added watch task #80
    also fixed quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
+var gulp = require('gulp');
+var babel = require('gulp-babel');
 
-gulp.task("build", ["build-client", "move-client", "build-server"]);
+gulp.task('build', ['build-client', 'move-client', 'build-server']);
 
-gulp.task("build-client", function () {
-	return gulp.src("client/js/*.js")
+gulp.task('build-client', function () {
+	return gulp.src('client/js/*.js')
 		.pipe(babel())
-		.pipe(gulp.dest("bin/client/js/"));
+		.pipe(gulp.dest('bin/client/js/'));
 });
 
-gulp.task("move-client", function () {
-	return gulp.src(["client/**/*.*", "!client/js/*.js"])
-		.pipe(gulp.dest("./bin/client/"));
+gulp.task('move-client', function () {
+	return gulp.src(['client/**/*.*', '!client/js/*.js'])
+		.pipe(gulp.dest('./bin/client/'));
 });
 
-gulp.task("build-server", function () {
-	return gulp.src("server/*.js")
+gulp.task('build-server', function () {
+	return gulp.src('server/*.js')
 		.pipe(babel())
-		.pipe(gulp.dest("bin/server/"));
+		.pipe(gulp.dest('bin/server/'));
 });
 
-
+gulp.task('watch', function () {
+	gulp.watch('client/**/*.*', ['build-client', 'move-client']);
+	gulp.watch('server/*.*', ['build-server']);
+});
