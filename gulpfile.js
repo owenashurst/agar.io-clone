0a819d42b0a421670c87048d869e5c6468c987ae
Usuń To <RemoveIt@users.noreply.github.com>
2015-05-31 20:47:51 +0200

    Building server #80

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
+gulp.task("build", ["build-client", "move-client", "build-server"]);
+
 gulp.task("build-client", function () {
-	gulp.src("client/js/*.js")
+	return gulp.src("client/js/*.js")
 		.pipe(babel())
 		.pipe(gulp.dest("bin/client/js/"));
 });
 
 gulp.task("move-client", function () {
-	gulp.src(["client/**/*.*", "!client/js/*.js"])
+	return gulp.src(["client/**/*.*", "!client/js/*.js"])
 		.pipe(gulp.dest("./bin/client/"));
 });
+
+gulp.task("build-server", function () {
+	return gulp.src("server/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("bin/server/"));
+});
+
+
