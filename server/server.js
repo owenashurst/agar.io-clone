d95030600275d4c55d11eed8d520af1a13ac1c57
Simon Matt <simonmatt1999@gmail.com>
2015-06-11 16:53:44 +0100

    Add Log Chat
    Chat can be easily logged via the server console.
    Settings can be configurated in "server/config.json".
    The Default is Zero, that means deactivated!
    To highlight the chat conversation, the Output in the server console begins with "[CHAT]".

--- server/server.js
+++ server/server.js
@@ -162,6 +162,9 @@ io.on('connection', function (socket) {
     socket.on('playerChat', function(data) {
         var _sender = data.sender.replace(/(<([^>]+)>)/ig, '');
         var _message = data.message.replace(/(<([^>]+)>)/ig, '');
+        if(c.logChat === 1) {
+        		console.log('[CHAT] [' + (new Date()).getHours() + ':' + (new Date()).getMinutes() + '] ' + _sender + ': ' + _message);
+        }
         socket.broadcast.emit('serverSendPlayerChat', {sender: _sender, message: _message});
     });
 
