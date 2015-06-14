0885a9261fed943e0b8e607b64db6013fb4c2072
Juha Tauriainen <juha@bin.fi>
2015-06-14 23:12:33 +0300

    Spaces to tabs in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,34 @@ var nodemon = require('gulp-nodemon');
 gulp.task('build', ['build-client', 'build-server']);
 
 gulp.task('build-client', ['move-client'], function () {
-	return gulp.src('client/js/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default', { verbose: true}))
-		.pipe(babel())
-		.pipe(gulp.dest('bin/client/js/'));
+  return gulp.src('client/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', { verbose: true}))
+    .pipe(babel())
+    .pipe(gulp.dest('bin/client/js/'));
 });
 
 gulp.task('move-client', function () {
-	return gulp.src(['client/**/*.*', '!client/js/*.js'])
-		.pipe(gulp.dest('./bin/client/'));
+  return gulp.src(['client/**/*.*', '!client/js/*.js'])
+    .pipe(gulp.dest('./bin/client/'));
 });
 
 gulp.task('build-server', ['move-server'], function () {
-	return gulp.src('server/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default', { verbose: true }))
-		.pipe(gulp.dest('bin/server/'));
+  return gulp.src('server/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', { verbose: true }))
+    .pipe(gulp.dest('bin/server/'));
 });
 
 gulp.task('move-server', function () {
-	return gulp.src(['server/**/*.*', '!server/**/*.js'])
-		.pipe(gulp.dest('./bin/server/'));
+  return gulp.src(['server/**/*.*', '!server/**/*.js'])
+    .pipe(gulp.dest('./bin/server/'));
 });
 
 gulp.task('watch', ["build"], function () {
-	gulp.watch('client/**/*.*', ['build-client', 'move-client']);
-	gulp.watch('server/*.*', ['build-server']);
-	gulp.start("run");
+  gulp.watch('client/**/*.*', ['build-client', 'move-client']);
+  gulp.watch('server/*.*', ['build-server']);
+  gulp.start("run");
 });
 
 gulp.task('run', ["build"], function () {
@@ -44,7 +44,7 @@ gulp.task('run', ["build"], function () {
         args: ["/server/config.json"],
         ext: 'html js css'
     })
-	  .on('restart', function () {
-	      console.log('restarted!');
-	  });
+    .on('restart', function () {
+        console.log('restarted!');
+    });
 });
