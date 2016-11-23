74a884c13d5be878df3ace5ac7ef2fd278f15b43
flaminggenius <livewiretech.reply.mail@gmail.com>
2016-11-23 14:01:44 -0500

    Update app.js
    Added listener for dark mode

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -140,6 +140,9 @@ continuitySetting.onchange = settings.toggleContinuity;
 var roundFoodSetting = document.getElementById('roundFood');
 roundFoodSetting.onchange = settings.toggleRoundFood;
 
+var darkModeSetting = document.getElementById ('darkMode');
+darkModeSetting.onchange = settings.toggleDarkMode;
+
 var c = window.canvas.cv;
 var graph = c.getContext('2d');
 
