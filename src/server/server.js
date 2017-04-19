2e32e22e3edc55a937b07cf37d491d6e0b1a0ea1
Vikram Durai <vikramvdurai@gmail.com>
2017-04-19 19:11:34 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -490,7 +490,12 @@ function tickPlayer(currentPlayer) {
         food[f] = {};
         food.splice(f, 1);
     }
-
+    
+    function deleteVirus(f){
+        virus[f] = {};
+        virus.splice(f, 1);
+    }
+    
     function eatMass(m) {
         if(SAT.pointInCircle(new V(m.x, m.y), playerCircle)){
             if(m.id == currentPlayer.id && m.speed > 0 && z == m.num)
@@ -566,7 +571,8 @@ function tickPlayer(currentPlayer) {
            .reduce( function(a, b, c) { return b ? a.concat(c) : a; }, []);
 
         if(virusCollision > 0 && currentCell.mass > virus[virusCollision].mass) {
-          sockets[currentPlayer.id].emit('virusSplit', z);
+            sockets[currentPlayer.id].emit('virusSplit', z);
+            virusCollision.forEach(deleteVirus);
         }
 
         var masaGanada = 0;
