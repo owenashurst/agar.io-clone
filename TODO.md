125a764dc3bdb3a9eaec23479e5699cd744c0827
abalabahaha <hi@abal.moe>
2017-05-11 23:07:53 -0700

    Dependency cleanup, ignore default SQL config

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ### TODOs
 | Filename | line # | TODO
 |:------|:------:|:------
-| server/server.js | 345 | Actually log incorrect passwords.
+| server/server.js | 364 | Actually log incorrect passwords.
 | client/js/app.js | 96 | Break out into GameControls.
 | client/js/chat-client.js | 24 | Break out many of these GameControls into separate classes.
--- src/server/server.js
+++ src/server/server.js
@@ -34,19 +34,21 @@ var leaderboardChanged = false;
 var V = SAT.Vector;
 var C = SAT.Circle;
 
-var pool = sql.createConnection({
-	host: s.host,
-	user: s.user,
-    password: s.password,
-	database: s.database
-});
+if(s.host !== "DEFAULT") {
+    var pool = sql.createConnection({
+        host: s.host,
+        user: s.user,
+        password: s.password,
+        database: s.database
+    });
 
-//log sql errors
-pool.connect(function(err){
-	if (err){
-		console.log (err);
-	}
-});
+    //log sql errors
+    pool.connect(function(err){
+        if (err){
+            console.log (err);
+        }
+    });
+}
 
 var initMassLog = util.log(c.defaultPlayerMass, c.slowBase);
 
