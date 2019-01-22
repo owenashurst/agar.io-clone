b262a9ce8c24f334f1e08d230d7f85fe87d03c5e
klslowik <klslowik@moontech.pl>
2019-01-22 19:05:24 +0100

    Update server.js (#517)
    Fix bug with undefined cell && cell.mass

--- src/server/server.js
+++ src/server/server.js
@@ -445,7 +445,7 @@ io.on('connection', function (socket) {
     });
     socket.on('2', function(virusCell) {
         function splitCell(cell) {
-            if(cell.mass >= c.defaultPlayerMass*2) {
+            if(cell && cell.mass && cell.mass >= c.defaultPlayerMass*2) {
                 cell.mass = cell.mass/2;
                 cell.radius = util.massToRadius(cell.mass);
                 currentPlayer.cells.push({
