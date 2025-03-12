c86f7c5f2f450b102818c810f110e77fef34f3ef
Roman Pisani <romanp2929@gmail.com>
2025-03-12 00:04:31 -0400

    sanitize player name (in case validNick is changed in the future)

--- src/server/server.js
+++ src/server/server.js
@@ -65,6 +65,10 @@ const addPlayer = (socket) => {
         } else {
             console.log('[INFO] Player ' + clientPlayerData.name + ' connected!');
             sockets[socket.id] = socket;
+
+            const sanitizedName = clientPlayerData.name.replace(/(<([^>]+)>)/ig, '');
+            clientPlayerData.name = sanitizedName;
+
             currentPlayer.clientProvidedData(clientPlayerData);
             map.players.pushNew(currentPlayer);
             io.emit('playerJoin', { name: currentPlayer.name });
